b03adab58609e5253c44ec3423ab28a3cb60fe60
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-06-29 11:14:51 -0700

    Create Automorphic-number.js

---
+++ Automorphic-number.js
@@ -0,0 +1,16 @@
+/*
+A number is called Automorphic number if and only 
+if its square ends in the same digits as the number itself.
+
+Task:
+  Given a number determine if it Automorphic or not.
+
+Notes:
+  The number passed to the function is positive
+  Input >> Output Examples
+
+Example:
+  autoMorphic (25) -->> return "Automorphic" 
+Explanation:
+  25 squared is 625 , Ends with the same number's digits which are 25 .
+*/
